a211ca34b5088cf9d0641c35e04f05095f17079c
Kyle Anderson <kyle@divspace.com>
2015-11-06 16:31:02 -0700

    Set the GET path

--- app/Http/Routes/ResumeRoute.php
+++ app/Http/Routes/ResumeRoute.php
@@ -5,7 +5,7 @@ use Illuminate\Contracts\Routing\Registrar;
 class ResumeRoute {
 
     public function map(Registrar $router) {
-        $router->get(null, 'ResumeController@index')->name('index');
+        $router->get('resume', 'ResumeController@index')->name('index');
     }
 
 }

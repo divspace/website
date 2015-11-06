f29faeaafabdc0480dd057707d9c94ec6597eccf
Kyle Anderson <kyle@divspace.com>
2015-11-06 16:39:27 -0700

    Resume dummy text

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.master')
 @section('content')
-    <h1>Resume</h1>
+    <div id="resume">
+        <h1>Resume</h1>
+        <p>Eum in noster senserit, per nonumy graeco consetetur in. Dolores sententiae efficiendi mea eu, eos eleifend postulant ne. Esse virtute senserit vix ut. Per at magna malorum explicari, admodum scriptorem has id. Has ex oblique salutandi posidonium, pro eripuit meliore ocurreret in. Reque deserunt recteque in eum, doming gloriatur te usu. Sententiae adversarium vix eu, cu sea salutatus incorrupte.</p>
+        <p>At iusto incorrupte nam, case placerat cum ea. Libris constituto adipiscing no cum, nam in quot quaeque. Velit viris ius ei, vix at agam vero accusata, eu mea mundi vocibus sententiae. Vix veri ferri oportere cu, per minim nullam ubique ei. Id cum tollit iriure eruditi, ex est elitr aperiam expetenda, cu mutat feugait percipitur sit.</p>
+        <p>Latine fierent vis an, ea eam accommodare concludaturque. Mundi graecis phaedrum ne qui, unum bonorum neglegentur cu qui. Et mei porro omittam. Ea nemore aeterno vim, vix quod tritani patrioque ei. Et dico delenit detraxit sit.</p>
+        <p>Scripta officiis ut vix. An facete denique has. Expetenda interesset an sit, ne eam dicat aperiam delicatissimi. Vim quando laoreet ut, ad mei accusata euripidis, te sea case platonem interpretaris. Eum vero facer impetus ne, pro nostrud scripserit ut.</p>
+    </div>
 @stop

4eaa61c4eb34fef54d0b24ad062d61f6aee1cba6
Kyle Anderson <kyle@divspace.com>
2015-11-07 16:54:22 -0700

    Spelling, added class names, screen layout changes

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.master')
 @section('content')
     <div id="resume">
-        <h1>Kyle Anderson</h1>
-        <ul class="contact">
+        <h1 class="resume-title">Kyle Anderson</h1>
+        <ul class="resume-contact">
             <li>Scottsdale, AZ</li>
             <li>(480) 434-4675</li>
             <li>kyle@divspace.com</li>
             <li>linkedin.com/in/divspace</li>
         </ul>
-        <h2>Web Developer &amp; Programmer</h2>
-        <table>
+        <h2 class="resume-occupation">Web Developer &amp; Programmer</h2>
+        <table class="resume-core-skills">
             <tr>
                 <td>PHP / HTML / CSS</td>
                 <td>Laravel / jQuery / Bootstrap</td>
@@ -17,40 +17,47 @@
                 <td>Technology Management</td>
             </tr>
         </table>
-        <p>
-            Versatile and efficiency focused front- / back-end developer and programmer.
-            <br>
-            Dedicated to building quality, high performance digital experiences, with a passion for translating complex requirements into elegant, functional code.
-        </p>
-        <p class="box">
-            Leverage technical expertise and creativity to innovate web properties that <strong>simplify complexity and enhance user experiences</strong>.
-            <br>
-            <strong>Thrive in both highly autonomous roles and collaborative team environments</strong> that are conducive to iteration and idea sharing.
-            <br>
-            Design and architect websites and web applications with a focus on user experiences and <strong>making information easier to grasp</strong>.
-        </p>
-        <h3>Professional Experience</h3>
-        <div class="experience">
+        <div class="resume-short-description">
+            <p>
+                Versatile and efficiency focused front- / back-end developer and programmer.
+            </p>
+            <p>
+                Dedicated to building quality, high performance digital experiences, with a passion for translating complex requirements into elegant, functional code.
+            </p>
+        </div>
+        <div class="resume-long-description">
+            <p>
+                Leverage technical expertise and creativity to innovate web properties that <strong>simplify complexity and enhance user experiences</strong>.
+            </p>
+            <p>
+                <strong>Thrive in both highly autonomous roles and collaborative team environments</strong> that are conducive to iteration and idea sharing.
+            </p>
+            <p>
+                Design and architect websites and web applications with a focus on user experiences and <strong>making information easier to grasp</strong>.
+            </p>
+        </div>
+        <h3 class="resume-section">Professional Experience</h3>
+        <div class="resume-job">
             <div class="row">
-                <div class="position col-xs-10">
+                <div class="col-xs-10 col-sm-8 col-md-8 col-lg-8 resume-job-position">
                     <strong>Application Developer</strong> &mdash; Media Solutions &mdash; Chandler, AZ
                 </div>
-                <div class="duration col-xs-2">
+                <div class="col-xs-2 col-sm-4 col-md-4 col-lg-4 resume-job-duration">
                     <em>2015 &mdash; Present</em>
                 </div>
             </div>
             <ul>
-                <li>Easily jumped into existing Laravel 4 and Laravel 5 projects and provided new ways to increase productivity by introduction Gulp for task automation, Bower for managing JavaScript and CSS libraries, and Sass for improved readability and organization for CSS.</li>
+                <li>Easily jumped into existing Laravel 4 and Laravel 5 projects and provided new ways to increase productivity by introducing Gulp for task automation, Bower for managing JavaScript and CSS libraries, and Sass for improved readability and organization for CSS.</li>
                 <li>Removed repetitive code and template variables with the DRY principle by creating repositories, transformers, presenters, and view composers.</li>
                 <li>Developed a program to automatically find missing product images and convert them web-friendly graphics for client websites.</li>
             </ul>
         </div>
-        <div class="experience">
+        <div class="resume-job">
             <div class="row">
-                <div class="position col-xs-10">
+                <div class="col-xs-10 col-sm-8 col-md-8 col-lg-8 resume-job-position">
                     <strong>Chief Technology Officer</strong> &mdash; Smart Source Hosting &mdash; New York, NY
                 </div>
-                <div class="duration col-xs-2">
+                <div class="col-xs-2 col-sm-4 col-md-4 col-lg-4 resume-job-duration">
                     <em>2014 &mdash; 2015</em>
                 </div>
             </div>
@@ -59,28 +66,28 @@
                 <li>Established Smart Source Hosting's social media presence and a high-ROI digital marketing strategy, including PPC, SEM, SMM, SEO, and email campaigns that achieved a 28% open rate.</li>
             </ul>
         </div>
-        <div class="experience">
+        <div class="resume-job">
             <div class="row">
-                <div class="position col-xs-10">
+                <div class="col-xs-10 col-sm-8 col-md-8 col-lg-8 resume-job-position">
                     <strong>Co-Founder</strong> &mdash; Crucial Web Hosting &mdash; Phoenix, AZ
                 </div>
-                <div class="duration col-xs-2">
+                <div class="col-xs-2 col-sm-4 col-md-4 col-lg-4 resume-job-duration">
                     <em>2006 &mdash; 2014</em>
                 </div>
             </div>
             <ul>
                 <li>Established and manage all aspects of the hosting businesses, from infrastructure development and maintenance to customer acquisition, web design and development, billing, and support.</li>
                 <li>Grew Crucial Web Hosting into $1MM+ in annual revenues through targeted marketing, consistent infrastructure modernization, and by providing outstanding user experiences.</li>
-                <li>Wrote blog and knowledge base articles to help clients and increase Crucial Web Hosting's online presence. Created content that earned prominent placement in industry publications, such as Smashing Magazine and A List Apart.</li>
+                <li>Wrote blog and knowledge base articles to help clients and increase Crucial Web Hosting's online presence. Created content that earned prominent placement in industry publications, such as <em>Smashing Magazine</em> and <em>A List Apart</em>.</li>
                 <li>Introduced Magento and established Crucial Web Hosting as the industry's most prominent hosting provider for the emerging ecommerce solution. Served as a technical product expert, which earned Crucial Web Hosting placement on Magento's wiki and differentiated the company.</li>
             </ul>
         </div>
-        <div class="experience">
+        <div class="resume-job">
             <div class="row">
-                <div class="position col-xs-10">
+                <div class="col-xs-10 col-sm-8 col-md-8 col-lg-8 resume-job-position">
                     <strong>IT Director</strong> &mdash; B&amp;D Litho &mdash; Phoenix, AZ
                 </div>
-                <div class="duration col-xs-2">
+                <div class="col-xs-2 col-sm-4 col-md-4 col-lg-4 resume-job-duration">
                     <em>2001 &mdash; 2002</em>
                 </div>
             </div>
@@ -89,62 +96,64 @@
                 <li>Developed B&amp;D Litho's online presence and introduced user-friendly technical enhancements to streamline the media submittal process and increase efficiency in the Graphic Design department.</li>
             </ul>
         </div>
-        <h3>Select Technical Expertise</h3>
-        <div class="row">
-            <div class="skill col-xs-3">Markup Languages</div>
-            <div class="items col-xs-7">HTML, Jade, YAML</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Stylesheet Languages</div>
-            <div class="items col-xs-7">CSS, Sass, SCSS, Stylus</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Programming Languages</div>
-            <div class="items col-xs-7">Bash, CoffeeScript, JavaScript, Perl, PHP, SQL</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Web Application Frameworks</div>
-            <div class="items col-xs-7">CodeIgniter, Laravel, Slim, Symfony, Yii, Zend Framework</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Front-End Frameworks</div>
-            <div class="items col-xs-7">Bootstrap, Font Awesome, Foundation</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">JavaScript Libraries</div>
-            <div class="items col-xs-7">jQuery, jQuery UI</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">JavaScript Frameworks</div>
-            <div class="items col-xs-7">AngularJS, Express.js, Node.js, Sails, Socket.IO</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Content Management Systems</div>
-            <div class="items col-xs-7">Drupal, ExpressionEngine, Ghost, Joomla, Magento, WordPress</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Package Managers</div>
-            <div class="items col-xs-7">Bower, Composer, CPAN, npm, PEAR</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Data Storage</div>
-            <div class="items col-xs-7">MariaDB, Memcached, MongoDB, MySQL, Percona Server, Redis</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Solution Stacks</div>
-            <div class="items col-xs-7">LAMP, LEMP, MEAN</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Version Control</div>
-            <div class="items col-xs-7">Git, Subversion</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Development Methods</div>
-            <div class="items col-xs-7">Kanban, Scrumban</div>
-        </div>
-        <div class="row">
-            <div class="skill col-xs-3">Development Tools</div>
-            <div class="items col-xs-7">EditorConfig, Gulp, Homestead, Sublime, Vim, VirtualBox</div>
+        <h3 class="resume-section">Select Technical Expertise</h3>
+        <div class="resume-skills">
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Markup Languages</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">HTML, Jade, YAML</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Stylesheet Languages</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">CSS, Sass, SCSS, Stylus</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Programming Languages</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Bash, CoffeeScript, JavaScript, Perl, PHP, SQL</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Web Application Frameworks</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">CodeIgniter, Laravel, Slim, Symfony, Yii, Zend Framework</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Front-End Frameworks</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Bootstrap, Font Awesome, Foundation</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">JavaScript Libraries</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">jQuery, jQuery UI</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">JavaScript Frameworks</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">AngularJS, Express.js, Node.js, Sails, Socket.IO</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Content Management Systems</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Drupal, ExpressionEngine, Ghost, Joomla, Magento, WordPress</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Package Managers</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Bower, Composer, CPAN, npm, PEAR</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Data Storage</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">MariaDB, Memcached, MongoDB, MySQL, Percona Server, Redis</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Solution Stacks</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">LAMP, LEMP, MEAN</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Version Control</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Git, Subversion</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Development Methods</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">Kanban, Scrumban</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3 resume-skill-section">Development Tools</div>
+                <div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 resume-skill-items">EditorConfig, Gulp, Homestead, Sublime, Vim, VirtualBox</div>
+            </div>
         </div>
     </div>
 @stop

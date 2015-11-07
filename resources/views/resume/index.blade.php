91df473d46674355b6ab1fd17b985dcfa735b462
Kyle Anderson <kyle@divspace.com>
2015-11-07 16:09:01 -0700

    Updated resume with content

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -1,10 +1,150 @@
 @extends('layouts.master')
 @section('content')
     <div id="resume">
-        <h1>Resume</h1>
-        <p>Eum in noster senserit, per nonumy graeco consetetur in. Dolores sententiae efficiendi mea eu, eos eleifend postulant ne. Esse virtute senserit vix ut. Per at magna malorum explicari, admodum scriptorem has id. Has ex oblique salutandi posidonium, pro eripuit meliore ocurreret in. Reque deserunt recteque in eum, doming gloriatur te usu. Sententiae adversarium vix eu, cu sea salutatus incorrupte.</p>
-        <p>At iusto incorrupte nam, case placerat cum ea. Libris constituto adipiscing no cum, nam in quot quaeque. Velit viris ius ei, vix at agam vero accusata, eu mea mundi vocibus sententiae. Vix veri ferri oportere cu, per minim nullam ubique ei. Id cum tollit iriure eruditi, ex est elitr aperiam expetenda, cu mutat feugait percipitur sit.</p>
-        <p>Latine fierent vis an, ea eam accommodare concludaturque. Mundi graecis phaedrum ne qui, unum bonorum neglegentur cu qui. Et mei porro omittam. Ea nemore aeterno vim, vix quod tritani patrioque ei. Et dico delenit detraxit sit.</p>
-        <p>Scripta officiis ut vix. An facete denique has. Expetenda interesset an sit, ne eam dicat aperiam delicatissimi. Vim quando laoreet ut, ad mei accusata euripidis, te sea case platonem interpretaris. Eum vero facer impetus ne, pro nostrud scripserit ut.</p>
+        <h1>Kyle Anderson</h1>
+        <ul class="contact">
+            <li>Scottsdale, AZ</li>
+            <li>(480) 434-4675</li>
+            <li>kyle@divspace.com</li>
+            <li>linkedin.com/in/divspace</li>
+        </ul>
+        <h2>Web Developer &amp; Programmer</h2>
+        <table>
+            <tr>
+                <td>PHP / HTML / CSS</td>
+                <td>Laravel / jQuery / Bootstrap</td>
+                <td>Systems Administration</td>
+                <td>Technology Management</td>
+            </tr>
+        </table>
+        <p>
+            Versatile and efficiency focused front- / back-end developer and programmer.
+            <br>
+            Dedicated to building quality, high performance digital experiences, with a passion for translating complex requirements into elegant, functional code.
+        </p>
+        <p class="box">
+            Leverage technical expertise and creativity to innovate web properties that <strong>simplify complexity and enhance user experiences</strong>.
+            <br>
+            <strong>Thrive in both highly autonomous roles and collaborative team environments</strong> that are conducive to iteration and idea sharing.
+            <br>
+            Design and architect websites and web applications with a focus on user experiences and <strong>making information easier to grasp</strong>.
+        </p>
+        <h3>Professional Experience</h3>
+        <div class="experience">
+            <div class="row">
+                <div class="position col-xs-10">
+                    <strong>Application Developer</strong> &mdash; Media Solutions &mdash; Chandler, AZ
+                </div>
+                <div class="duration col-xs-2">
+                    <em>2015 &mdash; Present</em>
+                </div>
+            </div>
+            <ul>
+                <li>Easily jumped into existing Laravel 4 and Laravel 5 projects and provided new ways to increase productivity by introduction Gulp for task automation, Bower for managing JavaScript and CSS libraries, and Sass for improved readability and organization for CSS.</li>
+                <li>Removed repetitive code and template variables with the DRY principle by creating repositories, transformers, presenters, and view composers.</li>
+                <li>Developed a program to automatically find missing product images and convert them web-friendly graphics for client websites.</li>
+            </ul>
+        </div>
+        <div class="experience">
+            <div class="row">
+                <div class="position col-xs-10">
+                    <strong>Chief Technology Officer</strong> &mdash; Smart Source Hosting &mdash; New York, NY
+                </div>
+                <div class="duration col-xs-2">
+                    <em>2014 &mdash; 2015</em>
+                </div>
+            </div>
+            <ul>
+                <li>Designed and built out Smart Source Hosting's entire technical infrastructure, including setting up Amazon Web Services (AWS) cloud hosting, implementing server hardening and security, identifying billing / payment processing solutions, and developing the site.</li>
+                <li>Established Smart Source Hosting's social media presence and a high-ROI digital marketing strategy, including PPC, SEM, SMM, SEO, and email campaigns that achieved a 28% open rate.</li>
+            </ul>
+        </div>
+        <div class="experience">
+            <div class="row">
+                <div class="position col-xs-10">
+                    <strong>Co-Founder</strong> &mdash; Crucial Web Hosting &mdash; Phoenix, AZ
+                </div>
+                <div class="duration col-xs-2">
+                    <em>2006 &mdash; 2014</em>
+                </div>
+            </div>
+            <ul>
+                <li>Established and manage all aspects of the hosting businesses, from infrastructure development and maintenance to customer acquisition, web design and development, billing, and support.</li>
+                <li>Grew Crucial Web Hosting into $1MM+ in annual revenues through targeted marketing, consistent infrastructure modernization, and by providing outstanding user experiences.</li>
+                <li>Wrote blog and knowledge base articles to help clients and increase Crucial Web Hosting's online presence. Created content that earned prominent placement in industry publications, such as Smashing Magazine and A List Apart.</li>
+                <li>Introduced Magento and established Crucial Web Hosting as the industry's most prominent hosting provider for the emerging ecommerce solution. Served as a technical product expert, which earned Crucial Web Hosting placement on Magento's wiki and differentiated the company.</li>
+            </ul>
+        </div>
+        <div class="experience">
+            <div class="row">
+                <div class="position col-xs-10">
+                    <strong>IT Director</strong> &mdash; B&amp;D Litho &mdash; Phoenix, AZ
+                </div>
+                <div class="duration col-xs-2">
+                    <em>2001 &mdash; 2002</em>
+                </div>
+            </div>
+            <ul>
+                <li>Managed all aspects of the company's technical infrastructure, including installing, updating, and administering hardware, systems software, applications software, and configurations. Constantly evaluated system to identify and address opportunities to improve reliability, availability, and security. Served as a technical support resource for end-users.</li>
+                <li>Developed B&amp;D Litho's online presence and introduced user-friendly technical enhancements to streamline the media submittal process and increase efficiency in the Graphic Design department.</li>
+            </ul>
+        </div>
+        <h3>Select Technical Expertise</h3>
+        <div class="row">
+            <div class="skill col-xs-3">Markup Languages</div>
+            <div class="items col-xs-7">HTML, Jade, YAML</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Stylesheet Languages</div>
+            <div class="items col-xs-7">CSS, Sass, SCSS, Stylus</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Programming Languages</div>
+            <div class="items col-xs-7">Bash, CoffeeScript, JavaScript, Perl, PHP, SQL</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Web Application Frameworks</div>
+            <div class="items col-xs-7">CodeIgniter, Laravel, Slim, Symfony, Yii, Zend Framework</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Front-End Frameworks</div>
+            <div class="items col-xs-7">Bootstrap, Font Awesome, Foundation</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">JavaScript Libraries</div>
+            <div class="items col-xs-7">jQuery, jQuery UI</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">JavaScript Frameworks</div>
+            <div class="items col-xs-7">AngularJS, Express.js, Node.js, Sails, Socket.IO</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Content Management Systems</div>
+            <div class="items col-xs-7">Drupal, ExpressionEngine, Ghost, Joomla, Magento, WordPress</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Package Managers</div>
+            <div class="items col-xs-7">Bower, Composer, CPAN, npm, PEAR</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Data Storage</div>
+            <div class="items col-xs-7">MariaDB, Memcached, MongoDB, MySQL, Percona Server, Redis</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Solution Stacks</div>
+            <div class="items col-xs-7">LAMP, LEMP, MEAN</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Version Control</div>
+            <div class="items col-xs-7">Git, Subversion</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Development Methods</div>
+            <div class="items col-xs-7">Kanban, Scrumban</div>
+        </div>
+        <div class="row">
+            <div class="skill col-xs-3">Development Tools</div>
+            <div class="items col-xs-7">EditorConfig, Gulp, Homestead, Sublime, Vim, VirtualBox</div>
+        </div>
     </div>
 @stop

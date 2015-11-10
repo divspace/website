62588a87894cb31f1a0199a2cb6ee90018630af8
Kyle Anderson <kyle@divspace.com>
2015-11-10 13:53:28 -0700

    Contact Form request

---
+++ app/Http/Requests/ContactFormRequest.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ContactFormRequest extends Request {
+
+    public function authorize() {
+        return true;
+    }
+
+    public function rules() {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|max:512',
+            'message' => 'required'
+        ];
+    }
+
+    public function messages() {
+        return [
+            'name.required' => 'Your name is required',
+            'email.required' => 'Your email address is required',
+            'email.email' => 'Your email address is invalid',
+            'email.max' => 'Your email address cannot exceed 512 characters',
+            'message.required' => 'Your message is required'
+        ];
+    }
+
+}

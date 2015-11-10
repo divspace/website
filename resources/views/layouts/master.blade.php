edea3328b3d083fb3b2235c3e5e6fd54e46cc2ca
Kyle Anderson <kyle@divspace.com>
2015-11-10 10:07:32 -0700

    Social links in navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,18 +11,31 @@
         <div class="container">
             <nav class="navbar navbar-light bg-faded">
                 <a class="navbar-brand" href="{{ url() }}" rel="home">
-                    <img class="logo" src="{{ asset('assets/img/logo_transparent.png') }}">
+                    <img class="logo" src="{{ asset('assets/img/logo-sm.png') }}" width="68" height="20">
                 </a>
                 <ul class="nav navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url() }}">Home <span class="sr-only">(Current)</span></a>
+                        <a class="nav-link{{ ($path === '/') ? ' active' : '' }}" href="{{ url() }}">Home <span class="sr-only">(Current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('resume') }}">Resume</a>
+                        <a class="nav-link{{ ($path === 'resume') ? ' active' : '' }}" href="{{ url('resume') }}">Resume</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#" data-toggle="modal" data-target="#contact">Contact</a>
                     </li>
+                </ul>
+                <ul class="nav navbar-nav pull-right">
+                    @foreach($socialLinks as $socialLink)
+                        @if($socialLink['show'] === true)
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ $socialLink['url'] }}" title="{{ $socialLink['title'] }}">
+                                    <span class="fa fa-fw fa-{{ $socialLink['icon'] }}">
+                                        <span class="sr-only">{{ $socialLink['title'] }}</span>
+                                    </span>
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
                 </ul>
             </nav>
         </div>

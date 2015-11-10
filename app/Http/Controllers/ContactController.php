2a22b7982c6140e929a2e92a5e07ebe72378b5d2
Kyle Anderson <kyle@divspace.com>
2015-11-10 13:57:38 -0700

    Contact form controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,9 +1,22 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactFormRequest;
+
 class ContactController extends Controller {
 
-    public function index() {
-        //
+    public function index(ContactFormRequest $request) {
+        return 'OK';
+
+        \Mail::send('emails.contact', [
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'message' => $request->get('message')
+        ], function($message) {
+            $message->from('postmaster@divspace.com');
+            $message->to('kyle@divspace.com', 'Kyle Anderson');
+            $message->subject('Divspace.com Contact Form Submission');
+        });
     }
 
 }

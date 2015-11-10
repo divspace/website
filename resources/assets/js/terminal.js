e38685cc01d887ff2297e43ac0f00fbb6e417165
Kyle Anderson <kyle@divspace.com>
2015-11-09 17:48:27 -0700

    Terminal JS

---
+++ resources/assets/js/terminal.js
@@ -0,0 +1,11 @@
+(function($) {
+  $('.output').typed({
+    contentType: 'html',
+    showCursor: false,
+    startDelay: 500,
+    strings: [
+        'php artisan serve --port=80'
+    ],
+    typeSpeed: 0
+  });
+})(jQuery);

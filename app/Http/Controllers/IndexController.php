19a14b8eac86cf35c688028e3a00887346c9d1f3
Kyle Anderson <kyle@divspace.com>
2015-11-10 13:52:03 -0700

    Add use declaration for controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
 class IndexController extends Controller {
 
     public function index() {
--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
 class ResumeController extends Controller {
 
     public function index() {

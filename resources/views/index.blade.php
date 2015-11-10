a6e2e93aeed6dbfd4d02e153b4e038144fc654d6
Kyle Anderson <kyle@divspace.com>
2015-11-09 17:48:44 -0700

    JS moved to file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,18 +41,3 @@
         </div>
     </div>
 @stop
-@section('script')
-    <script>
-        $(function() {
-            $('.output').typed({
-                contentType: 'html',
-                showCursor: false,
-                startDelay: 500,
-                strings: [
-                    'php artisan serve --port=80'
-                ],
-                typeSpeed: 0
-            });
-        });
-    </script>
-@append

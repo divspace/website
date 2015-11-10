71c8599d2555b07de9a79a962e82eeb2cae45d36
Kyle Anderson <kyle@divspace.com>
2015-11-10 13:54:56 -0700

    Contact form view

--- resources/views/forms/_contact.blade.php
+++ resources/views/forms/_contact.blade.php
@@ -9,7 +9,7 @@
                 <h4 class="modal-title" id="contact-form-title">Contact</h4>
             </div>
             <div class="modal-body">
-                <form action="{{ url('contact') }}" method="POST">
+                <form id="contact-form" action="{{ url('contact') }}" method="POST">
                     <div class="form-group">
                         <label for="name">Name</label>
                         <input class="form-control" id="name" name="name" type="text">
@@ -22,11 +22,10 @@
                         <label for="message">Message</label>
                         <textarea class="form-control" id="message" name="message" rows="5"></textarea>
                     </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary pull-right" type="submit">Send</button>
+                    </div>
                 </form>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <button class="btn btn-primary" type="button">Send</button>
             </div>
         </div>
     </div>

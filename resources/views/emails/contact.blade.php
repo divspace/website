fd8f55e72acd01d59afd82d0026d2690f1b488c5
Kyle Anderson <kyle@divspace.com>
2015-11-10 17:11:17 -0700

    New contact form submission template

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,2 +1,26 @@
-<p>From: {{ $name }} ({{ $email }})</p>
-<p>{{ $message }}</p>
+<table width="100%" border="0" cellpadding="2" cellspacing="0">
+    <tr>
+        <td width="10%" valign="top">
+            <strong>Name</strong>
+        </td>
+        <td width="90%" valign="top">
+            {{ $name }}
+        </td>
+    </tr>
+    <tr>
+        <td width="10%" valign="top">
+            <strong>Email</strong>
+        </td>
+        <td width="90%" valign="top">
+            {{ $email }}
+        </td>
+    </tr>
+    <tr>
+        <td width="10%" valign="top">
+            <strong>Message</strong>
+        </td>
+        <td width="90%" valign="top">
+            {{ $body }}
+        </td>
+    </tr>
+</table>

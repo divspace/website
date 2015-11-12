9e3a8074b1c2c7e41eeb0398945225109f3abbfc
Kyle Anderson <kyle@divspace.com>
2015-11-12 16:43:36 -0700

    Put fonts in own variable

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,16 @@ var assets = {
   js: './public/assets/js'
 };
 
+var fonts = {
+  hack: './bower_components/hack/fonts',
+  sourceSansPro: './bower_components/source-sans-pro/fonts',
+  sourceSerifPro: './bower_components/source-serif-pro/fonts'
+};
+
 var packages = {
   bootstrap: './bower_components/bootstrap/dist/js',
   formValidation: './resources/vendor/form-validation',
-  hack: './bower_components/hack/fonts',
   jquery: './bower_components/jquery/dist',
-  sourceSansPro: './bower_components/source-sans-pro/fonts',
-  sourceSerifPro: './bower_components/source-serif-pro/fonts',
   typedJs: './bower_components/typed-js/js'
 };
 
@@ -27,9 +30,9 @@ elixir(function(mix) {
     .copy('./resources/assets/img/**', assets.img)
 
     // Fonts
-    .copy(packages.hack + '/**', assets.fonts)
-    .copy(packages.sourceSansPro + '/**', assets.fonts)
-    .copy(packages.sourceSerifPro + '/**', assets.fonts)
+    .copy(fonts.hack + '/**', assets.fonts)
+    .copy(fonts.sourceSansPro + '/**', assets.fonts)
+    .copy(fonts.sourceSerifPro + '/**', assets.fonts)
 
     // SCSS
     .sass('app.scss', assets.css)

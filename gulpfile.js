7c6ea6ed54bf5472b04dd277d4f0adff3ebe7ff9
Kyle Anderson <kyle@divspace.com>
2015-11-12 13:19:05 -0700

    Added new fonts

--- gulpfile.js
+++ gulpfile.js
@@ -14,20 +14,41 @@ var packages = {
   formValidation: './resources/vendor/form-validation',
   hack: './bower_components/hack/build/webfonts',
   jquery: './bower_components/jquery/dist',
+  ptSerif: './bower_components/pt-serif/fonts',
+  sourceSansPro: './bower_components/source-sans-pro/fonts',
   typedJs: './bower_components/typed-js/js'
 };
 
 elixir(function(mix) {
   mix
     .clear([assets.css, assets.fonts, assets.img, assets.js])
+
+    // Images
     .copy('./resources/assets/img/**', assets.img)
-    .copy('./resources/assets/fonts/**', assets.fonts)
-    .copy(packages.hack + '/fonts/woff2/latin/**', assets.fonts)
+
+    // Hack
+    .copy(packages.hack + '/fonts/eot/latin/*.eot', assets.fonts + '/hack')
+    .copy(packages.hack + '/fonts/svg/latin/*.svg', assets.fonts + '/hack')
+    .copy(packages.hack + '/fonts/web-ttf/latin/*.ttf', assets.fonts + '/hack')
+    .copy(packages.hack + '/fonts/woff/latin/*.woff', assets.fonts + '/hack')
+    .copy(packages.hack + '/fonts/woff2/latin/*.woff2', assets.fonts + '/hack')
+
+    // PT Serif
+    .copy(packages.ptSerif + '/**', assets.fonts)
+
+    // Source Sans Pro
+    .copy(packages.sourceSansPro + '/**', assets.fonts)
+
+    // SCSS
     .sass('app.scss', assets.css)
+
+    // CSS
     .styles([
       packages.formValidation + '/css/form-validation.css',
       assets.css + '/app.css'
     ], assets.css + '/app.css')
+
+    // JavaScript
     .scripts([
       packages.jquery + '/jquery.js',
       packages.bootstrap + '/bootstrap.js',

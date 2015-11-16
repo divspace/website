2a192019a8a2b6fa18b38428ec3506b4e575ed3b
Kyle Anderson <kyle@divspace.com>
2015-11-16 10:41:30 -0700

    Test page

---
+++ app/Http/Routes/TestRoute.php
@@ -0,0 +1,11 @@
+<?php namespace App\Http\Routes;
+
+use Illuminate\Contracts\Routing\Registrar;
+
+class TestRoute {
+
+    public function map(Registrar $router) {
+        $router->get('test', 'TestController@index')->name('index');
+    }
+
+}

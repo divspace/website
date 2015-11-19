9024e3ebe68f9e8c95c051f90c25b134880e2432
Kyle Anderson <kyle@divspace.com>
2015-11-19 12:58:48 -0700

    Fixed request

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Request;
 
 class ApiController extends Controller {
 

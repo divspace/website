9b14c04d45354e0a606399fa058a32bc2a38ad55
Kyle Anderson <kyle@divspace.com>
2015-11-19 12:27:14 -0700

    New voicemail message

---
+++ resources/views/emails/voicemail.blade.php
@@ -0,0 +1,34 @@
+<table width="100%" border="0" cellpadding="2" cellspacing="0">
+  <tr>
+    <td width="15%" valign="top">
+      <strong>From</strong>
+    </td>
+    <td width="85%" valign="top">
+      {{ $from }}
+    </td>
+  </tr>
+  <tr>
+    <td valign="top">
+      <strong>Fowarded From</strong>
+    </td>
+    <td valign="top">
+      {{ $forwardedFrom }}
+    </td>
+  </tr>
+  <tr>
+    <td valign="top">
+      <strong>Message</strong>
+    </td>
+    <td valign="top">
+      {{ $message }}
+    </td>
+  </tr>
+  <tr>
+    <td valign="top">
+      <strong>Duration</strong>
+    </td>
+    <td valign="top">
+      {{ $duration }}
+    </td>
+  </tr>
+</table>

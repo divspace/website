750f9f9c7aafce45e1a57eedabdae0ab76d02e12
Kyle Anderson <kyle@divspace.com>
2015-11-19 17:03:08 -0700

    Obfuscate email using PHP instead of CSS

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -5,7 +5,28 @@ use App\Http\Controllers\Controller;
 class ResumeController extends Controller {
 
     public function index() {
-        return view('resume.index');
+        $email = $this->obfuscateEmail('kyle@divspace.com');
+
+        return view('resume.index', compact('email'));
+    }
+
+    private function obfuscateEmail($string) {
+        $result = '';
+        $encode = array('.', ':', '@');
+
+        for($i = 0; $i < strlen($string); ++$i) {
+            if(in_array($string[$i], $encode) || mt_rand(1, 100) < 50) {
+                if(mt_rand(0, 1)) {
+                    $result .= '&#'.ord($string[$i]).';';
+                } else {
+                    $result .= '&#x'.dechex(ord($string[$i])).';';
+                }
+            } else {
+                $result .= $string[$i];
+            }
+        }
+
+        return $result;
     }
 
 }

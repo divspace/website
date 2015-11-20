de367b770747da8b35671b4eb6046976dcb41cfa
Kyle Anderson <kyle@divspace.com>
2015-11-19 17:20:57 -0700

    Moved obfuscate function to Helpers/Helpers.php

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -5,28 +5,7 @@ use App\Http\Controllers\Controller;
 class ResumeController extends Controller {
 
     public function index() {
-        $email = $this->obfuscateEmail('kyle@divspace.com');
-
-        return view('resume.index', compact('email'));
-    }
-
-    private function obfuscateEmail($string) {
-        $result = '';
-        $encode = array('.', ':', '@');
-
-        for($i = 0; $i < strlen($string); ++$i) {
-            if(in_array($string[$i], $encode) || mt_rand(1, 100) < 50) {
-                if(mt_rand(0, 1)) {
-                    $result .= '&#'.ord($string[$i]).';';
-                } else {
-                    $result .= '&#x'.dechex(ord($string[$i])).';';
-                }
-            } else {
-                $result .= $string[$i];
-            }
-        }
-
-        return $result;
+        return view('resume.index');
     }
 
 }

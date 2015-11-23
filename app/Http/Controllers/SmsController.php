d0472abb7c3b6a8f87403adcb56e2ee6cc15a1fe
Kyle Anderson <kyle@divspace.com>
2015-11-23 13:57:53 -0700

    Outgoing SMS

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -7,18 +7,36 @@ class SmsController extends Controller {
 
     public function incoming(Request $request) {
         if($request->input('From') === env('PHONE_NUMBER')) {
-            $message = 'Hey, Kyle';
+            $xml  = '<Response>';
+            $xml .= '  <Message action="/sms/outgoing" method="POST" />';
+            $xml .= '</Response>';
         } else {
             $message = $request->input('From').' - '.htmlspecialchars($request->input('Body'));
+
+            $xml  = '<Response>';
+            $xml .= '  <Message to="'.env('PHONE_NUMBER').'">';
+            $xml .=      $message;
+            $xml .= '  </Message>';
+            $xml .= '</Response>';
         }
 
-        $xml  = '<Response>';
-        $xml .= '  <Message to="'.env('PHONE_NUMBER').'">';
-        $xml .=      $message;
-        $xml .= '  </Message>';
-        $xml .= '</Response>';
-
         return response($xml, 200)->header('Content-Type', 'text/xml');
+    }
+
+    public function outgoing(Request $request) {
+        if(preg_match('/\+[0-9]{10}/', $request->input('Body'), $phoneNumbers)) {
+            $phoneNumber = $phoneNumbers[1];
+
+            $message = 'Sent from my iPhone';
+
+            $xml  = '<Response>';
+            $xml .= '  <Message to="'.$phoneNumber.'" from="'.env('TWILIO_NUMBER').'">';
+            $xml .=      $message;
+            $xml .= '  </Message>';
+            $xml .= '</Response>';
+
+            return response($xml, 200)->header('Content-Type', 'text/xml');
+        }
     }
 
 }
--- app/Http/Routes/SmsRoute.php
+++ app/Http/Routes/SmsRoute.php
@@ -6,6 +6,7 @@ class SmsRoute {
 
     public function map(Registrar $router) {
         $router->post('sms/incoming', 'SmsController@incoming')->name('sms.incoming');
+        $router->post('sms/outgoing', 'SmsController@outgoing')->name('sms.outgoing');
     }
 
 }

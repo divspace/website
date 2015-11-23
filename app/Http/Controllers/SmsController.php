bfe6d06ea0d0ba9e4afe9bc6bd26f85f3b065df6
Kyle Anderson <kyle@divspace.com>
2015-11-23 15:31:46 -0700

    If I reply, send SMS from Twilio number

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -8,17 +8,14 @@ class SmsController extends Controller {
     public function incoming(Request $request) {
         if($request->input('From') === env('PHONE_NUMBER')) {
             $xml  = '<Response>';
-            $xml .= '  <Redirect method="POST">';
-            $xml .=      route('sms.outgoing');
-            $xml .= '  </Redirect>';
+            $xml .= '  <Redirect method="POST">'.route('sms.outgoing').'</Redirect>';
             $xml .= '</Response>';
         } else {
-            $message = $request->input('From').' - '.htmlspecialchars($request->input('Body'));
+            $message  = 'From: '.$request->input('From')."\n";
+            $message .= htmlspecialchars($request->input('Body'));
 
             $xml  = '<Response>';
-            $xml .= '  <Message to="'.env('PHONE_NUMBER').'">';
-            $xml .=      $message;
-            $xml .= '  </Message>';
+            $xml .= '  <Message to="'.env('PHONE_NUMBER').'">'.$message.'</Message>';
             $xml .= '</Response>';
         }
 
@@ -26,12 +23,10 @@ class SmsController extends Controller {
     }
 
     public function outgoing(Request $request) {
-        $message = $request->input('From').' - '.$request->input('Body').' - Sent from my iPhone';
+        $message = htmlspecialchars($request->input('Body'));
 
         $xml  = '<Response>';
-        $xml .= '  <Message to="'.$request->input('From').'" from="'.env('TWILIO_NUMBER').'">';
-        $xml .=      $message;
-        $xml .= '  </Message>';
+        $xml .= '  <Message to="'.$request->input('From').'" from="'.env('TWILIO_NUMBER').'">'.$message.'</Message>';
         $xml .= '</Response>';
 
         return response($xml, 200)->header('Content-Type', 'text/xml');

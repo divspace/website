8df357b2472da3558888f39df06a985f026b842a
Kyle Anderson <kyle@divspace.com>
2015-12-09 13:02:38 -0700

    User and Shift transformers

--- app/Transformers/ShiftTransformer.php
+++ app/Transformers/ShiftTransformer.php
@@ -10,8 +10,16 @@ class ShiftTransformer extends TransformerAbstract {
     protected $availableIncludes = [];
     protected $defaultIncludes = [];
 
-    public function transform($resource) {
-        //
+    public function transform($shift) {
+        return [
+            'id' => $shift->id,
+            'user_id' => $shift->user_id,
+            'break' => $shift->break,
+            'start_time' => $shift->start_time,
+            'end_time' => $shift->end_time,
+            'created_at' => $shift->created_at,
+            'updated_at' => $shift->updated_at
+        ];
     }
 
 }
--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -10,8 +10,16 @@ class UserTransformer extends TransformerAbstract {
     protected $availableIncludes = [];
     protected $defaultIncludes = [];
 
-    public function transform($resource) {
-        //
+    public function transform($user) {
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'role' => $user->role,
+            'email' => $user->email,
+            'phone' => $user->phone,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at
+        ];
     }
 
 }
